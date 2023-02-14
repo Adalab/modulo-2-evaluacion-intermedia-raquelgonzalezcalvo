16d11ee3f150a1474e19e3a6b31ff193c4308eb3
raquelgonzalezcalvo <ragoncal13@gmail.com>
2023-02-14 11:00:01 +0100

    add functions

---
+++ js/main.js
@@ -0,0 +1,47 @@
+'use strict'
+
+//constantes
+const inputUser = document.querySelector('.js-usernumber');
+const button = document.querySelector ('.js-btn');
+const spaceClue = document.querySelector ('.js-clue');
+const spaceAttempts = document.querySelector ('js-attempts');
+
+
+// funciones
+
+function getRandomNumber(max) {
+ return Math.ceil(Math.random() * max);
+}
+
+const randomvalue= getRandomNumber (100)
+console.log(randomvalue);
+
+function handlerNumber () {
+  const inputUserValue = parseInt(inputUser.value);
+  console.log (inputUserValue);
+
+  if (inputUserValue < 1 || inputUserValue > 100) { 
+    spaceClue.innerHTML = "El número debe de estar entre 1 y 100"
+  } else if(inputUserValue > randomvalue) {
+    spaceClue.innerHTML = "Demasiado alto";
+  } else if(inputUserValue < randomvalue) {
+    spaceClue.innerHTML = "Demasiado bajo";
+  } else (inputUserValue = randomvalue); {
+    spaceClue.innerHTML = "Has ganado campeona";
+  }   
+}
+
+let counter = 0;
+function counter() {
+ 
+}
+
+
+function hadleClick(event) {
+  event.preventDefault()
+  handlerNumber()
+  counter()
+}
+
+// eventos
+button.addEventListener('click', hadleClick);
